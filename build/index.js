69d8567b34303d48f0dfede192b5976a98e79c1a
Gijsjan <agijsbro@gmail.com>
2015-07-21 17:16:18 +0200

    Turn Input into es6 class

--- build/index.js
+++ build/index.js
@@ -5,7 +5,15 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
 
 var _react = _dereq_("react");
 
@@ -15,88 +23,116 @@ var _classnames = _dereq_("classnames");
 
 var _classnames2 = _interopRequireDefault(_classnames);
 
-var Input = _react2["default"].createClass({
-	displayName: "Input",
+var Input = (function (_React$Component) {
+	function Input(props) {
+		_classCallCheck(this, Input);
 
-	propTypes: {
-		onChange: _react2["default"].PropTypes.func,
-		onInvalid: _react2["default"].PropTypes.func,
-		onKeyDown: _react2["default"].PropTypes.func,
-		onKeyUp: _react2["default"].PropTypes.func,
-		placeholder: _react2["default"].PropTypes.string,
-		style: _react2["default"].PropTypes.object,
-		valid: _react2["default"].PropTypes.bool,
-		validate: _react2["default"].PropTypes.func,
-		value: _react2["default"].PropTypes.oneOfType([_react2["default"].PropTypes.string, _react2["default"].PropTypes.number])
-	},
+		_get(Object.getPrototypeOf(Input.prototype), "constructor", this).call(this, props);
 
-	getDefaultProps: function getDefaultProps() {
-		return {
-			value: ""
+		this.state = {
+			valid: true,
+			invalidMessage: null
 		};
-	},
-
-	getInitialState: function getInitialState() {
-		return {
-			valid: true
-		};
-	},
-
-	componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
-		if (this.props.value === nextProps.value) {
-			return;
-		}
-
-		if (nextProps.value === "") {
-			if (!this.state.valid) {
-				this.setState({ valid: true });
-			}
-
-			return;
-		}
-
-		if (this.props.validate) {
-			var validator = this.props.validate(nextProps.value);
-
-			this.setState({ valid: validator.isValid });
-
-			if (!validator.isValid && this.props.onInvalid) {
-				this.props.onInvalid(validator.message, nextProps.value);
-			}
-		}
-	},
-
-	shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextState) {
-		return this.props.value !== nextProps.value;
-	},
-
-	handleKeyDown: function handleKeyDown(ev) {
-		if (this.props.onKeyDown) {
-			this.props.onKeyDown(ev);
-		}
-	},
-
-	handleKeyUp: function handleKeyUp(ev) {
-		if (this.props.onKeyUp) {
-			this.props.onKeyUp(ev);
-		}
-	},
-
-	handleChange: function handleChange(ev) {
-		this.props.onChange(ev.currentTarget.value, ev);
-	},
-
-	render: function render() {
-		return _react2["default"].createElement("input", {
-			className: (0, _classnames2["default"])("hire-input", { invalid: !this.state.valid }),
-			onChange: this.handleChange,
-			onKeyDown: this.handleKeyDown,
-			onKeyUp: this.handleKeyUp,
-			placeholder: this.props.placeholder,
-			style: this.props.style,
-			value: this.props.value });
 	}
-});
+
+	_inherits(Input, _React$Component);
+
+	_createClass(Input, [{
+		key: "componentWillReceiveProps",
+		value: function componentWillReceiveProps(nextProps) {
+			if (this.props.value === nextProps.value) {
+				return;
+			}
+
+			if (nextProps.value === "") {
+				if (!this.state.valid) {
+					this.setState({
+						valid: true,
+						invalidMessage: null
+					});
+				}
+
+				return;
+			} else if (this.props.validate) {
+				var validator = this.props.validate(nextProps.value);
+
+				this.setState({
+					valid: validator.isValid,
+					invalidMessage: validator.message
+				});
+
+				if (!validator.isValid && this.props.onInvalid) {
+					this.props.onInvalid(validator.message, nextProps.value);
+				}
+			}
+		}
+	}, {
+		key: "shouldComponentUpdate",
+		value: function shouldComponentUpdate(nextProps, nextState) {
+			return this.props.value !== nextProps.value;
+		}
+	}, {
+		key: "handleKeyDown",
+		value: function handleKeyDown(ev) {
+			if (this.props.onKeyDown) {
+				this.props.onKeyDown(ev);
+			}
+		}
+	}, {
+		key: "handleKeyUp",
+		value: function handleKeyUp(ev) {
+			if (this.props.onKeyUp) {
+				this.props.onKeyUp(ev);
+			}
+		}
+	}, {
+		key: "handleChange",
+		value: function handleChange(ev) {
+			this.props.onChange(ev.currentTarget.value, ev);
+		}
+	}, {
+		key: "render",
+		value: function render() {
+			var invalidMessage = this.state.invalidMessage ? _react2["default"].createElement(
+				"div",
+				{ className: "hire-forms-invalid-message" },
+				this.state.invalidMessage
+			) : null;
+
+			return _react2["default"].createElement(
+				"div",
+				{
+					className: (0, _classnames2["default"])("hire-input", { invalid: !this.state.valid }) },
+				_react2["default"].createElement("input", {
+					onChange: this.handleChange.bind(this),
+					onKeyDown: this.handleKeyDown.bind(this),
+					onKeyUp: this.handleKeyUp.bind(this),
+					placeholder: this.props.placeholder,
+					style: this.props.style,
+					value: this.props.value }),
+				invalidMessage
+			);
+		}
+	}]);
+
+	return Input;
+})(_react2["default"].Component);
+
+Input.propTypes = {
+	onChange: _react2["default"].PropTypes.func,
+	onInvalid: _react2["default"].PropTypes.func,
+	onKeyDown: _react2["default"].PropTypes.func,
+	onKeyUp: _react2["default"].PropTypes.func,
+	placeholder: _react2["default"].PropTypes.string,
+	style: _react2["default"].PropTypes.object,
+	valid: _react2["default"].PropTypes.bool,
+	validate: _react2["default"].PropTypes.func,
+	value: _react2["default"].PropTypes.oneOfType([_react2["default"].PropTypes.string, _react2["default"].PropTypes.number])
+};
+
+Input.defaultProps = {
+	value: ""
+};
 
 exports["default"] = Input;
 module.exports = exports["default"];
--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,15 @@
 import React from "react";
 import cx from "classnames";
 
-let Input = React.createClass({
-	propTypes: {
-		onChange: React.PropTypes.func,
-		onInvalid: React.PropTypes.func,
-		onKeyDown: React.PropTypes.func,
-		onKeyUp: React.PropTypes.func,
-		placeholder: React.PropTypes.string,
-		style: React.PropTypes.object,
-		valid: React.PropTypes.bool,
-		validate: React.PropTypes.func,
-		value: React.PropTypes.oneOfType([
-			React.PropTypes.string,
-			React.PropTypes.number
-		])
-	},
+class Input extends React.Component {
+	constructor(props) {
+		super(props);
 
-	getDefaultProps() {
-		return {
-			value: ""
+		this.state = {
+			valid: true,
+			invalidMessage: null
 		};
-	},
-
-	getInitialState() {
-		return {
-			valid: true
-		};
-	},
+	}
 
 	componentWillReceiveProps(nextProps) {
 		if (this.props.value === nextProps.value) {
@@ -36,58 +18,88 @@ let Input = React.createClass({
 
 		if (nextProps.value === "") {
 			if (!this.state.valid) {
-				this.setState({valid: true});
+				this.setState({
+					valid: true,
+					invalidMessage: null
+				});
 			}
 
 			return;
-		}
-
-		if (this.props.validate) {
+		} else if (this.props.validate) {
 			let validator = this.props.validate(nextProps.value);
 
-			this.setState({valid: validator.isValid});
+			this.setState({
+				valid: validator.isValid,
+				invalidMessage: validator.message
+			});
 
 			if (!validator.isValid && this.props.onInvalid) {
 				this.props.onInvalid(validator.message, nextProps.value);
 			}
 		}
-	},
+	}
 
 	shouldComponentUpdate(nextProps, nextState) {
 		return this.props.value !== nextProps.value;
-	},
+	}
 
 	handleKeyDown(ev) {
 		if (this.props.onKeyDown) {
 			this.props.onKeyDown(ev);
 		}
-	},
+	}
 
 	handleKeyUp(ev) {
 		if (this.props.onKeyUp) {
 			this.props.onKeyUp(ev);
 		}
-	},
+	}
 
 	handleChange(ev) {
 		this.props.onChange(ev.currentTarget.value, ev);
-	},
+	}
 
 	render() {
+		let invalidMessage = this.state.invalidMessage ?
+			<div className="hire-forms-invalid-message">{this.state.invalidMessage}</div> :
+			null;
+
 		return (
-			<input
+			<div
 				className={cx(
 					"hire-input",
 					{invalid: !this.state.valid}
-				)}
-				onChange={this.handleChange}
-				onKeyDown={this.handleKeyDown}
-				onKeyUp={this.handleKeyUp}
-				placeholder={this.props.placeholder}
-				style={this.props.style}
-				value={this.props.value} />
+				)}>
+				<input
+					onChange={this.handleChange.bind(this)}
+					onKeyDown={this.handleKeyDown.bind(this)}
+					onKeyUp={this.handleKeyUp.bind(this)}
+					placeholder={this.props.placeholder}
+					style={this.props.style}
+					value={this.props.value} />
+				{invalidMessage}
+			</div>
 		);
 	}
-});
+}
+
+Input.propTypes = {
+	onChange: React.PropTypes.func,
+	onInvalid: React.PropTypes.func,
+	onKeyDown: React.PropTypes.func,
+	onKeyUp: React.PropTypes.func,
+	placeholder: React.PropTypes.string,
+	style: React.PropTypes.object,
+	valid: React.PropTypes.bool,
+	validate: React.PropTypes.func,
+	value: React.PropTypes.oneOfType([
+		React.PropTypes.string,
+		React.PropTypes.number
+	])
+};
+
+Input.defaultProps = {
+	value: ""
+};
 
 export default Input;
