0600181dd59521374f29374187996cd9b1c448db
Gijsjan <agijsbro@gmail.com>
2015-07-21 17:16:53 +0200

    Write tests WiP

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,3 +1,4 @@
+jest.dontMock("classnames");
 jest.dontMock("../src/index.jsx");
 
 import React from "react/addons";
@@ -6,9 +7,18 @@ import Input from "../src/index.jsx";
 
 
 describe("HireForms Input", function() {
-	it("Shoul do", function() {
-		let Output = {};
+	it("Should be a ReactElement", function() {
+		expect(TestUtils.isElement(<Input />)).toBeTruthy();
+	});
+
+	it("Should transfer value property to input value", function() {
+		let component = TestUtils.renderIntoDocument(
+			<Input value="my value" />
+		);
+
+		let input = TestUtils.findRenderedDOMComponentWithTag(component, "input");
+		console.log(input);
 
 		expect(TestUtils.isElement(<Input />)).toBeTruthy();
-	})
+	});
 });
