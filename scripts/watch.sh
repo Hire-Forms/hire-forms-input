ba195f6bbc03021f49fee88faa5c67c284658594
renevanderark <r.van.der.ark@gmail.com>
2016-02-09 14:02:37 +0100

    minimal port for react 0.14

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -5,6 +5,7 @@ node_modules/.bin/watchify src/index.jsx \
   --extension=.jsx \
   --external classnames \
   --external react \
+  --external react-dom \
   --outfile 'derequire > build/index.js' \
   --standalone HireFormsInput \
   --transform [ babelify --plugins object-assign ] \
